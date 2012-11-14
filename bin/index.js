1fe4ca87f7d90224f9d46e7186b859f5e501829d
remixz <zbruggeman@me.com>
2012-11-13 20:25:52 -0800

    Improve CLI commands.

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
-var argv = require('optimist').demand(['i', 't']).argv,
+var optimist = require('optimist'),
     wizardry = require('./../lib/wizardry');
 
 var callback = function(err) {
     if (err) { console.dir(err); };
 }
+
+var argv = optimist.usage('Usage: $0 [images] [task]')
+    .demand(['i', 't'])
+    .alias('i', 'image')
+    .alias('t', 'task')
+    .describe('i', 'Image(s) to be processed.')
+    .describe('t', 'Task file used to process images.')
+    .argv;
 
 wizardry([__dirname + '/' + argv.i], require('./' + argv.t), callback);
 
